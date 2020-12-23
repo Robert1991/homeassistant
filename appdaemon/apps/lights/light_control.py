186afc656b1c4a7d31990cf962fc517e249544b3
Robert Waddell <rwaddell91@googlemail.com>
2020-12-23 16:46:27 +0100

    added first python light automation including tests

--- appdaemon/apps/lights/light_control.py
+++ appdaemon/apps/lights/light_control.py
@@ -4,8 +4,43 @@ from time import sleep
 
 
 class DimLights(hass.Hass):
-    def read_state_as_float(self, entity):
-        return float(self.get_state(entity))
+    def initialize(self):
+        self.listen_state(self.toggle_event,
+                          self.args["light_group"], new="on")
+        self.listen_state(self.toggle_event,
+                          self.args["light_group"], attribute="brightness")
+        self.listen_state(self.toggle_event, self.args["light_threshold"])
+        self.listen_state(self.toggle_event,
+                          self.args["enable_automation_input"])
+
+    def toggle_event(self, entity, attribute, old, new, kwargs):
+        if not self.check_if_light_needs_to_be_dimmed(entity):
+            return
+
+        lights_in_group = self.get_state(
+            self.args["light_group"], attribute="entity_id")
+        turned_off_lights = set()
+        sleep(1)
+        while self.read_light_sensor_state() > self.read_light_threshold() and len(turned_off_lights) < len(lights_in_group):
+            for light in lights_in_group:
+                if self.get_state(light) == "on":
+                    new_light_brightness = self.calculate_new_light_brightness(
+                        light)
+                    if new_light_brightness > self.args["light_turn_off_step_size"]:
+                        self.turn_on(light, brightness=new_light_brightness)
+                    else:
+                        turned_off_lights.add(light)
+                        self.turn_off(light)
+                else:
+                    turned_off_lights.add(light)
+            self.log(turned_off_lights)
+            sleep(1)
+
+    def check_if_light_needs_to_be_dimmed(self, entity):
+        light_group_is_on = self.get_state(self.args["light_group"]) == "on"
+        automation_is_enabled = self.get_state(
+            self.args["enable_automation_input"]) == 'on'
+        return light_group_is_on and automation_is_enabled
 
     def read_light_sensor_state(self):
         return self.read_state_as_float(self.args["light_sensor"])
@@ -13,30 +48,10 @@ class DimLights(hass.Hass):
     def read_light_threshold(self):
         return self.read_state_as_float(self.args["light_threshold"])
 
-    def initialize(self):
-        self.listen_state(self.toggle_event,
-                          self.args["light_group"], new="on")
-        self.listen_state(self.toggle_event, self.args["light_threshold"])
+    def read_state_as_float(self, entity):
+        return float(self.get_state(entity))
 
-    def toggle_event(self, entity, attribute, old, new, kwargs):
-        if entity == self.args["light_threshold"] and self.get_state(self.args["light_group"]) == "off":
-            return
-
-        group_name = self.args["light_group"].replace("group.", "")
-        lights_in_group = self.entities.group[group_name].attributes.entity_id
-        turned_off_lights = set()
-        sleep(1)
-        while self.read_light_sensor_state() > self.read_light_threshold() and len(turned_off_lights) < len(lights_in_group):
-            for light in lights_in_group:
-                if self.get_state(light) == "on":
-                    current_light_brightness = self.get_state(
-                        light, attribute="brightness")
-                    if (current_light_brightness - 5) > 1:
-                        self.turn_on(light, brightness=(
-                            current_light_brightness - 5))
-                    else:
-                        turned_off_lights.add(light)
-                else:
-                    turned_off_lights.add(light)
-            self.log(turned_off_lights)
-            sleep(1)
+    def calculate_new_light_brightness(self, light_entity_id):
+        current_light_brightness = self.get_state(
+            light_entity_id, attribute="brightness")
+        return float(current_light_brightness - self.args["light_turn_off_step_size"])
---
+++ appdaemon/tests/light_control_test.py
@@ -0,0 +1,148 @@
+from apps.lights.light_control import DimLights
+from appdaemon.plugins.hass.hassapi import Hass
+from mock import patch, MagicMock, Mock
+import mock
+import pytest
+import unittest
+from appdaemontestframework import automation_fixture
+from appdaemontestframework import hass_mocks
+
+import pytest
+
+
+def pytest_configure():
+    pytest.LIGHT_GROUP_ARG = 'light_group'
+
+
+@automation_fixture(DimLights)
+def dim_lights(given_that):
+    given_that.passed_arg('light_group').is_set_to('group.some_light_group')
+    given_that.passed_arg('light_sensor').is_set_to(
+        'sensor.light_intensity_in_percent')
+    given_that.passed_arg('light_threshold').is_set_to(
+        'input_number.some_input_slider')
+    given_that.passed_arg('light_turn_off_step_size').is_set_to(
+        20)
+    given_that.passed_arg('light_turn_off_boundary_brightness').is_set_to(
+        20)
+    given_that.passed_arg('enable_automation_input').is_set_to(
+        'input_boolean.automation_enabled')
+    given_that.state_of('input_boolean.automation_enabled').is_set_to('on')
+
+
+def test_toggle_event_when_automation_is_disabled(given_that, dim_lights, assert_that):
+    given_that.state_of('group.some_light_group').is_set_to('on')
+    given_that.state_of('input_boolean.automation_enabled').is_set_to('off')
+    dim_lights.toggle_event(
+        'input_number.some_input_slider', None, None, None, None)
+    assert_that('light.some_light').was_not.turned_on()
+
+
+def test_toggle_event_when_light_group_is_off(given_that, dim_lights, assert_that):
+    given_that.state_of('group.some_light_group').is_set_to('off')
+    dim_lights.toggle_event(
+        'input_number.some_input_slider', None, None, None, None)
+    assert_that('light.some_light').was_not.turned_on()
+
+
+def test_toggle_event_when_current_light_intensity_less_than_threshold(given_that, dim_lights, assert_that):
+    given_that.state_of("light.some_light").is_set_to("on")
+    given_that.state_of('group.some_light_group').is_set_to('on',
+                                                            {'entity_id': ["light.some_light"]})
+    given_that.state_of('sensor.light_intensity_in_percent').is_set_to('70.0')
+    given_that.state_of('input_number.some_input_slider').is_set_to('80.0')
+    dim_lights.toggle_event(
+        'group.some_light_group', None, None, None, None)
+    assert_that('light.some_light').was_not.turned_on()
+
+
+called_light_sensor = 0
+dim_light_iteration = 0
+
+
+def resetGlobals():
+    globals()["called_light_sensor"] = 0
+    globals()["dim_light_iteration"] = 0
+
+
+def fetch_from_globals_with_count_increase(return_args, global_key):
+    returned_in_this_iteration = return_args[globals()[global_key]]
+    globals()[global_key] = globals()[global_key] + 1
+    return returned_in_this_iteration
+
+
+def test_toggle_light_when_light_intensity_is_over_threshold_light_is_dimmed(given_that, dim_lights, assert_that):
+    def get_state_patched(*args, **kwargs):
+        entity_name = args[0]
+        len_kwargs = len(kwargs)
+
+        if entity_name == 'input_boolean.automation_enabled':
+            return 'on'
+        if entity_name == 'group.some_light_group':
+            if len_kwargs == 0:
+                return 'on'
+            elif kwargs['attribute'] == 'entity_id':
+                return ['light.some_light']
+        if entity_name == 'light.some_light':
+            if len_kwargs == 0:
+                return 'on'
+            elif kwargs['attribute'] == 'brightness':
+                return_args = [100, 80]
+                return fetch_from_globals_with_count_increase(return_args, 'dim_light_iteration')
+        if entity_name == 'input_number.some_input_slider':
+            return 90.0
+        if entity_name == 'sensor.light_intensity_in_percent':
+            return_args = [95.0, 92.0, 85.0]
+            return fetch_from_globals_with_count_increase(return_args, 'called_light_sensor')
+        return "None"
+
+    with patch('appdaemon.plugins.hass.hassapi.Hass.get_state', side_effect=get_state_patched):
+        resetGlobals()
+        dim_lights.toggle_event(
+            'group.some_light_group', None, None, None, None)
+        assert globals()['called_light_sensor'] == 3
+        assert_that('light.some_light').was.turned_on(brightness=80)
+        assert_that('light.some_light').was.turned_on(brightness=60)
+
+
+dim_light_iteration_light_1 = 0
+dim_light_iteration_light_2 = 0
+
+
+def test_toggle_light_stop_to_dim_when_all_lights_off(given_that, dim_lights, assert_that):
+    def get_state_patched(*args, **kwargs):
+        entity_name = args[0]
+        len_kwargs = len(kwargs)
+
+        if entity_name == 'input_boolean.automation_enabled':
+            return 'on'
+        if entity_name == 'group.some_light_group':
+            if len_kwargs == 0:
+                return 'on'
+            elif kwargs['attribute'] == 'entity_id':
+                return ['light.some_light', 'light.some_light_2']
+        if entity_name == 'light.some_light_2':
+            if len_kwargs == 0:
+                return_args = ['on', 'off']
+                return fetch_from_globals_with_count_increase(return_args, 'dim_light_iteration_light_2')
+            elif kwargs['attribute'] == 'brightness':
+                return 30
+        if entity_name == 'light.some_light':
+            if len_kwargs == 0:
+                return 'on'
+            elif kwargs['attribute'] == 'brightness':
+                return_args = [50, 30]
+                return fetch_from_globals_with_count_increase(return_args, 'dim_light_iteration_light_1')
+        if entity_name == 'input_number.some_input_slider':
+            return 90.0
+        if entity_name == 'sensor.light_intensity_in_percent':
+            return_args = [95.0, 92.0, 91.0]
+            return fetch_from_globals_with_count_increase(return_args, 'called_light_sensor')
+        return "None"
+    with patch('appdaemon.plugins.hass.hassapi.Hass.get_state', side_effect=get_state_patched):
+        resetGlobals()
+        dim_lights.toggle_event(
+            'group.some_light_group', None, None, None, None)
+        assert_that('light.some_light').was.turned_on(brightness=30)
+        assert_that('light.some_light').was.turned_off()
+        assert_that('light.some_light_2').was.turned_off()
