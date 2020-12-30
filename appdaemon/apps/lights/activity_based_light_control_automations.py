580d1b51484c388f596b2569a999ddbdea0c91b1
Robert Waddell <rwaddell91@googlemail.com>
2020-12-30 15:57:25 +0100

    added light turn off automation in python

--- appdaemon/apps/lights/activity_based_light_control_automations.py
+++ appdaemon/apps/lights/activity_based_light_control_automations.py
@@ -1,6 +1,27 @@
 import appdaemon.plugins.hass.hassapi as hass
 
 
+class TurnOffAutomation(hass.Hass):
+    current_timer = None
+
+    def initialize(self):
+        self.listen_state(self.start_turn_off_lights_timer,
+                          self.args["observed_activity_sensor"], new="off")
+        self.listen_state(self.stop_turn_off_lights_timer,
+                          self.args["observed_activity_sensor"], new="on")
+
+    def start_turn_off_lights_timer(self, entity, attribute, old, new, kwargs):
+        timeout = int(float(self.get_state(self.args["turn_off_timeout"])))
+        self.current_timer = self.run_in(
+            self.turn_off_light_group, int(timeout))
+
+    def stop_turn_off_lights_timer(self, entity, attribute, old, new, kwargs):
+        self.cancel_timer(self.current_timer)
+
+    def turn_off_light_group(self, kwargs):
+        self.turn_off(self.args["light_group"])
+
+
 class TurnOnAutomation(hass.Hass):
     def initialize(self):
         self.listen_state(self.turn_on_lights,
--- appdaemon/tests/activity_based_light_control_automations_test.py
+++ appdaemon/tests/activity_based_light_control_automations_test.py
@@ -1,4 +1,5 @@
 from apps.lights.activity_based_light_control_automations import TurnOnAutomation
+from apps.lights.activity_based_light_control_automations import TurnOffAutomation
 from appdaemontestframework import automation_fixture
 from mock import patch
 from datetime import datetime
@@ -108,10 +109,6 @@ def test_turn_on_lights_when_there_is_movement_and_sufficient_lights(given_that,
     given_that.state_of(
         'input_select.some_scene_input_select').is_set_to('Relaxed Light')
 
-    def now_is_between_patched(from_time, to_time):
-        if from_time == "09:00:00" and to_time == "23:00:00":
-            return True
-        return False
     with patch('appdaemon.plugins.hass.hassapi.Hass.now_is_between', side_effect=now_is_between_patched_return_true):
         turn_on_lights.turn_on_lights(
             'binary_sensor.some_activity_sensor', None, None, None, None)
@@ -149,3 +146,30 @@ def test_turn_on_lights_when_there_is_movement_and_insufficient_but_time_span_no
             'binary_sensor.some_activity_sensor', None, None, None, None)
 
         assert_that('scene.some_room_relaxed_light').was_not.turned_on()
+
+
+@automation_fixture(TurnOffAutomation)
+def turn_off_lights(given_that):
+    given_that.passed_arg('observed_activity_sensor').is_set_to(
+        'binary_sensor.some_activity_sensor')
+    given_that.passed_arg('turn_off_timeout').is_set_to(
+        'input_number.some_turn_off_time_out')
+    given_that.passed_arg('light_group').is_set_to('light.some_light_group')
+
+
+def test_start_turn_off_lights_timer_turns_off_light_after_configured_input(given_that, turn_off_lights, assert_that, time_travel):
+    given_that.state_of('input_number.some_turn_off_time_out') \
+        .is_set_to("179.0")
+    turn_off_lights.start_turn_off_lights_timer(None, None, None, None, None)
+    time_travel.fast_forward(3).minutes()
+    assert_that('light.some_light_group').was.turned_off()
+
+
+def test_start_turn_off_lights_timer_not_turns_off_when_movement_occurs(given_that, turn_off_lights, assert_that, time_travel):
+    given_that.state_of('input_number.some_turn_off_time_out') \
+        .is_set_to("179.0")
+    turn_off_lights.start_turn_off_lights_timer(None, None, None, None, None)
+    time_travel.fast_forward(1).minutes()
+    turn_off_lights.stop_turn_off_lights_timer(None, None, None, None, None)
+    time_travel.fast_forward(2).minutes()
+    assert_that('light.some_light_group').was_not.turned_off()
