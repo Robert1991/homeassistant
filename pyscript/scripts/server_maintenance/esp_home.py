27c5f46295e90b51725f9e57b7893a183ccf21a8
Robert1991 <rwaddell91@googlemail.com>
2021-07-29 20:00:38 +0200

    added esp home pyscript services for updates

--- pyscript/scripts/server_maintenance/esp_home.py
+++ pyscript/scripts/server_maintenance/esp_home.py
@@ -11,20 +11,20 @@ def update_all_esp_home_devices():
 
 @service
 def run_esp_home_device_update(esp_home_device_config):
-    esp_home_base_path = pyscript.config["pyscript"]["global"]["esp_home_automations"]["esp_home_base_path"]
+    esp_home_base_path = pyscript.config["global"]["esp_home_automations"]["esp_home_base_path"]
 
-    esp_home_device_docker_run_command = "docker run --rm -v " + esp_home_base_path + ":/config esphome/esphome " + \
+    esp_home_device_docker_run_command = "docker run --rm -v " + esp_home_base_path + ":/config esphome/esphome " \
         + esp_home_device_config + " run --no-logs"
     log.info("Running esp-home update for: '" + esp_home_device_config +
              "' with command line: " + esp_home_device_docker_run_command)
     command_result = task.executor(
         run_remote_shell_command,
         esp_home_device_docker_run_command,
-        pyscript.config["pyscript"]["global"]["host_server"]["ssh_login"],
-        pyscript.config["pyscript"]["global"]["host_server"]["ssh_key"])
+        pyscript.config["global"]["host_server"]["ssh_login"],
+        pyscript.config["global"]["host_server"]["ssh_key"])
     if command_result.returncode != 0:
         log.error("Running esp home update for " +
-                  esp_home_device_config + " failed:\\n:" + str(command_result.stderr))
+                  esp_home_device_config + " failed:\\n" + str(command_result.stderr))
         notify.persistent_notification(title="ESP Home Device Update Failed", message="Device update for " +
                                        esp_home_device_config + " failed. Check logs for more information.")
     else:
